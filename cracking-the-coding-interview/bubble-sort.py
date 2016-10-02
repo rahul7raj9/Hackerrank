7b25c6060f2a09d1a325fc9cca97c129c3155450
RAHUL RAJ <rahul7.raj9@gmail.com>
2016-10-02 11:58:46 -0400

    Create bubble-sort.py

---
+++ cracking-the-coding-interview/bubble-sort.py
@@ -0,0 +1,13 @@
+n = int(raw_input().strip())
+a = map(int, raw_input().strip().split(' '))
+no_swaps=0
+for j in range(n):
+    for i in range(n-1):
+        if a[i]>a[i+1]:
+            a[i]=a[i]+a[i+1]
+            a[i+1]=a[i]-a[i+1]
+            a[i]=a[i]-a[i+1]
+            no_swaps+=1
+print "Array is sorted in" ,no_swaps, "swaps."
+print "First Element:",a[0]
+print "Last Element:",a[-1]
